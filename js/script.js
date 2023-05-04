3a82091fef7d3d9f53d68895f4d5e2cda9a4ff30
Fayzullayev Imomali <fayzullayevimomali@mail.ru>
2023-05-04 18:03:28 +0500

    Slider

--- js/script.js
+++ js/script.js
@@ -206,6 +206,54 @@ imgTarget.forEach(function(img) {
     imgObserver.observe(img);
 });
 
+//Slider 
+
+const slides = document.querySelectorAll('.slide');
+
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+
+let currentSlide = 0;
+const maxSlide = slides.length;
+
+// Next slide
+const goToSlide = function(slide) {
+    slides.forEach((s, i)=> s.style.transform = `translateX(${100 * (i - slide)}%)`)
+}
+
+goToSlide(0);
+const nextSlide = function() {
+    if(currentSlide === maxSlide - 1) {
+        currentSlide = 0;
+    } else {
+        currentSlide++;
+    }
+    goToSlide(currentSlide);
+}
+
+const prevSlide = function() {
+    if(currentSlide === 0) {
+        currentSlide = maxSlide - 1;
+    } else{
+        currentSlide--;
+    }
+
+    goToSlide(currentSlide);
+}
+
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
+
+document.addEventListener('keydown', function(e) {
+    if(e.key === "ArrowRight") {
+        nextSlide();
+    } else if(e.key === "ArrowLeft") {
+        
+    }
+});
+
+
+
 
 //Vladini Minin tasks
 
@@ -214,7 +262,6 @@ const isUnique = function(str) {
 }
 
 isUnique('abcd');
-
 
 
 
